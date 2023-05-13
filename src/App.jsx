b9b356859d47ac3a5847cb3f0f3b2a25212d5337
Masum Rahman <masumrahman73@gmail.com>
2023-05-14 01:38:14 +0600

    M68 - project installation completed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-6xl text-center my-20 text-purple-600'>Hot Hot Cold Coffee:</h1>
+      
+    </>
+  )
+}
+
+export default App
